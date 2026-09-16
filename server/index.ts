4cd17fa0a6dbdb7d30c9469c7f0ed8e218e1b5dc
CedricDevCode <cedric.karamoko@gmail.com>
2026-09-16 16:30:35 +0000

    fix: CSP workerSrc blob: pour pdf.js - contrat charge sur desktop

--- server/index.ts
+++ server/index.ts
@@ -112,6 +112,7 @@ app.use(helmet({
       styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
       fontSrc: ["'self'", 'https://fonts.gstatic.com'],
       imgSrc: ["'self'", 'data:', 'blob:', 'https:'],
+      workerSrc: ["'self'", 'blob:'],
       connectSrc: ["'self'", 'https://exacademie.net', 'https://www.exacademie.net', 'https://*.hostingersite.com', ...(frontendUrl ? [frontendUrl] : [])],
     },
   },
